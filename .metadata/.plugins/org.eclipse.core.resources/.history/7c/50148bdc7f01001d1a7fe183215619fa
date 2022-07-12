a03a98b838af6b0ea2c6c3eff1d721b76dbfc9a3
moonjaeh <mooooo1997@naver.com>
2022-07-12 16:56:03 +0900

    20220712

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7c/50148bdc7f01001d1a7fe183215619fa
@@ -0,0 +1,9 @@
+package com.spring.boardweb.service;
+
+import java.util.List;
+
+import com.spring.boardweb.HomeVO;
+
+public interface HomeService {
+	List<HomeVO> getNameList();
+}
---
+++ _01_BoardWeb_properties/src/main/java/com/spring/boardweb/service/impl/HomeServiceImpl.java
@@ -0,0 +1,39 @@
+package com.spring.boardweb.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.boardweb.HomeVO;
+import com.spring.boardweb.service.HomeService;
+
+//어노테이션으로 별칭을 만들어 줄수 있다.
+//별칭없이 생성하면 클래스명에서 첫 글자만 소문자로 변경된 이름으로 객체생성
+@Service
+public class HomeServiceImpl implements HomeService {
+	@Autowired
+	HomeDAO homeDAO;
+	
+	@Override
+	public List<HomeVO> getNameList() {
+		return homeDAO.getNameList();
+	}
+
+	@Override
+	public void insertName (HomeVO homeVO) {
+		homeDAO.insertName(homeVO);
+	}
+
+	@Override
+	public void updateName(Map<String, String> paramMap) {
+		// TODO Auto-generated method stub
+		homeDAO.updateName(paramMap);
+	}
+	@Override
+	public void deleteName(Map<String, String> paramMap) {
+		// TODO Auto-generated method stub
+		homeDAO.deleteName(paramMap);
+	}
+}
